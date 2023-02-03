196fbdd48ce1be5f0002a155d13e65f4b31d658c
Ru2Lu <jiutianlian0@gmail.com>
2023-02-03 13:52:50 +0900

    Assing1 Finished.

---
+++ flutter_course_assignment_1/lib/text_control.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './text_output.dart';
+
+class TextControl extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TextControlState();
+  }
+}
+
+class _TextControlState extends State<TextControl> {
+  String _mainText = 'This is the first assingment!';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _mainText = 'This changed!';
+              });
+            },
+            child: Text('Change Text')),
+        TextOutput(_mainText),
+      ],
+    );
+  }
+}
