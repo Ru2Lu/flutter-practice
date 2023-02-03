6b7686de7d2c9a690683a5f66b3fe4d348dba3c6
Ru2Lu <jiutianlian0@gmail.com>
2023-02-03 21:29:47 +0900

    Formatted date using external package

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -63,7 +64,7 @@ class MyHomePage extends StatelessWidget {
                       width: 2,
                     )),
                     padding: EdgeInsets.all(10),
-                    child: Text(tx.amount.toString(),
+                    child: Text('\$${tx.amount}',
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
@@ -76,7 +77,7 @@ class MyHomePage extends StatelessWidget {
                           style: TextStyle(
                               fontSize: 16, fontWeight: FontWeight.bold)),
                       Text(
-                        tx.date.toString(),
+                        DateFormat.yMMMd().format(tx.date),
                         style: TextStyle(color: Colors.grey),
                       ),
                     ],
